228056a94e641a8f193f73d316fabe57292bd098
andi-makes <andi@andi-makes.dev>
2022-10-01 11:57:58 +0200

    Update to 1.19

--- src/main/java/dev/schmarrn/lighty/Render.java
+++ src/main/java/dev/schmarrn/lighty/Render.java
@@ -8,6 +8,7 @@ import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.client.world.ClientWorld;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Box;
+import net.minecraft.util.math.random.LocalRandom;
 
 import java.util.List;
 import java.util.Random;
@@ -42,7 +43,7 @@ public class Render {
                     matrixStack,
                     provider.getBuffer(RenderLayer.getTranslucent()),
                     false,
-                    random
+                    new LocalRandom(1337)
             );
 
             matrixStack.pop();
