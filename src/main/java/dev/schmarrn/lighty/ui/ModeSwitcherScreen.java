3cd8bb624ff06447ab3a9970dca6bfcc68231195
andi-makes <andi@andi-makes.dev>
2023-01-03 15:32:54 +0100

    Tweaked UI spacings to look more like 1.19.3

--- src/main/java/dev/schmarrn/lighty/ui/ModeSwitcherScreen.java
+++ src/main/java/dev/schmarrn/lighty/ui/ModeSwitcherScreen.java
@@ -12,28 +12,33 @@ import java.util.List;
 
 public class ModeSwitcherScreen extends Screen {
     private static final List<ButtonWidget> BUTTONS = Lists.newArrayList();
+
+    private static final int START_HEIGHT = 15;
+    private static final int DELTA_HEIGHT = 24;
+
     public ModeSwitcherScreen() {
         super(Text.of("Mode Switcher"));
     }
 
     @Override
     protected void init() {
-        this.addDrawableChild(new ButtonWidget(this.width/2 - 75, this.height/6 + 48 - 6, 150, 20, ScreenTexts.OFF, button -> Lighty.mode = null));
-        int height = this.height/6 + 48 - 6 + 24;
+        int height = START_HEIGHT + DELTA_HEIGHT;
+        this.addDrawableChild(new ButtonWidget(this.width/2 - 75, height, 150, 20, ScreenTexts.OFF, button -> Lighty.mode = null));
         for (ButtonWidget btn : BUTTONS) {
+            height += DELTA_HEIGHT;
             btn.x = this.width / 2 - 75;
             btn.y = height;
-            height += 24;
             this.addDrawableChild(btn);
         }
 
+        height += DELTA_HEIGHT;
         this.addDrawableChild(new ButtonWidget(this.width/2 - 75, height + 6, 150, 20, ScreenTexts.DONE, button -> this.close()));
     }
 
     @Override
     public void render(MatrixStack matrices, int mouseX, int mouseY, float delta) {
         renderBackground(matrices);
-        drawCenteredText(matrices, this.textRenderer, this.title, this.width/2, 15, 0xFFFFFF);
+        drawCenteredText(matrices, this.textRenderer, this.title, this.width/2, START_HEIGHT, 0xFFFFFF);
         super.render(matrices, mouseX, mouseY, delta);
     }
 
