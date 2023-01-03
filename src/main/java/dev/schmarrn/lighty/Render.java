37b526733986f0a39ad63cedec822e4f4df6030e
andi-makes <andi@andi-makes.dev>
2023-01-03 19:10:00 +0100

    Lots of changes, see commit message
     - Decoupled computing overlay from the render loop, which results in
       much better performance
     - Added ModeCache class, which provides an easy way of computing
       changes while rendering
     - Added beforeCompute and afterCompute callbacks to LightyMode
       interface - useful for using the buffer
     - Refactoring of Render and Compute class to accomodate the new
       changes.

--- src/main/java/dev/schmarrn/lighty/Render.java
+++ src/main/java/dev/schmarrn/lighty/Render.java
@@ -2,12 +2,10 @@ package dev.schmarrn.lighty;
 
 import net.fabricmc.fabric.api.client.rendering.v1.WorldRenderContext;
 import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.network.ClientPlayerEntity;
 import net.minecraft.client.render.Frustum;
 import net.minecraft.client.render.Tessellator;
 import net.minecraft.client.render.VertexConsumerProvider;
 import net.minecraft.client.world.ClientWorld;
-import net.minecraft.util.math.BlockPos;
 
 public class Render {
     private static final VertexConsumerProvider.Immediate provider = VertexConsumerProvider.immediate(Tessellator.getInstance().getBuffer());
@@ -16,17 +14,12 @@ public class Render {
     static void renderOverlay(WorldRenderContext worldRenderContext) {
         if (Lighty.mode == null) return;
 
-        ClientPlayerEntity player = client.player;
         ClientWorld world = client.world;
         Frustum frustum = worldRenderContext.frustum();
 
-        if (player == null || world == null || frustum == null) {
+        if (world == null || frustum == null) {
             return;
         }
-
-        BlockPos playerPos = new BlockPos(player.getPos());
-        // fixme: remove this from the render call
-        Compute.computeCache(playerPos, world);
 
         // Render
         Lighty.mode.render(worldRenderContext, world, frustum, provider, client);
--- src/main/java/dev/schmarrn/lighty/mode/LightyMode.java
+++ src/main/java/dev/schmarrn/lighty/mode/LightyMode.java
@@ -1,39 +1,18 @@
 package dev.schmarrn.lighty.mode;
 
 import net.fabricmc.fabric.api.client.rendering.v1.WorldRenderContext;
-import net.minecraft.block.BlockState;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.render.Frustum;
 import net.minecraft.client.render.VertexConsumerProvider;
 import net.minecraft.client.world.ClientWorld;
 import net.minecraft.util.math.BlockPos;
-import org.apache.commons.compress.utils.Lists;
-
-import java.util.List;
 
 public abstract class LightyMode {
-    public static class Data {
-        BlockPos pos;
-        BlockState state;
-        double offset;
+    public void beforeCompute() {}
 
-        Data(BlockPos pos, BlockState state, double offset) {
-            this.pos = pos;
-            this.state = state;
-            this.offset = offset;
-        }
-    }
-    protected final List<Data> cache = Lists.newArrayList();
+    public void afterCompute() {}
 
     public abstract void compute(ClientWorld world, BlockPos pos);
 
     public abstract void render(WorldRenderContext worldRenderContext, ClientWorld world, Frustum frustum, VertexConsumerProvider.Immediate provider, MinecraftClient client);
-
-    public boolean shouldReCompute() {
-        return cache.isEmpty();
-    }
-
-    public void clearCache() {
-        cache.clear();
-    }
 }
