30a87ad105fdbc6b20dacf4ad826036c93258325
andi-makes <andi@andi-makes.dev>
2023-01-05 10:35:59 +0100

    Implemented first version of a Number Mode
     - Workaround a Bug regarding not marking Compute as dirty, resulting in not
       instantanious loading of the desired mode
     - Refactored Colors into their own class for possible future
       customization and for other Modes to reuse already existing colors.
     - Implemented a Number Mode

--- src/main/java/dev/schmarrn/lighty/Blocks.java
+++ src/main/java/dev/schmarrn/lighty/Blocks.java
@@ -8,9 +8,9 @@ import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
 
 public class Blocks {
-    public static final Block GREEN_OVERLAY = registerOverlayBlock("green_overlay", 0x00FF00);
-    public static final Block ORANGE_OVERLAY = registerOverlayBlock("orange_overlay", 0xFF6600);
-    public static final Block RED_OVERLAY = registerOverlayBlock("red_overlay", 0xFF0000);
+    public static final Block GREEN_OVERLAY = registerOverlayBlock("green_overlay", LightyColors.GREEN);
+    public static final Block ORANGE_OVERLAY = registerOverlayBlock("orange_overlay", LightyColors.ORANGE);
+    public static final Block RED_OVERLAY = registerOverlayBlock("red_overlay", LightyColors.RED);
 
     private static Block registerOverlayBlock(String name, int color) {
         Block overlay = new OverlayBlock(AbstractBlock.Settings.of(Material.WOOL), color);
