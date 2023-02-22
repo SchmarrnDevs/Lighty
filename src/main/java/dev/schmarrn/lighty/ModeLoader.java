a086460c67e8cfd3b4382606f7ae8c0dfd7b29e5
andi-makes <andi@andi-makes.dev>
2023-02-22 14:04:06 +0100

    Added Toggle KeyBind

--- src/main/java/dev/schmarrn/lighty/ModeLoader.java
+++ src/main/java/dev/schmarrn/lighty/ModeLoader.java
@@ -11,23 +11,41 @@ public class ModeLoader {
     @Nullable
     private static LightyMode<?, ?> mode = null;
 
+    private static boolean enabled = false;
+
     private static final HashMap<Identifier, LightyMode<?, ?>> MODES = new HashMap<>();
 
     public static void loadMode(LightyMode<?, ?> mode) {
-        if (ModeLoader.mode != null) {
+        if (enabled) {
             ModeLoader.mode.unload();
         }
 
-        ModeLoader.mode = mode;
-        Compute.markDirty();
+        if (mode == null) {
+            enabled = false;
+        } else {
+            enabled = true;
+            ModeLoader.mode = mode;
+            Compute.markDirty();
+        }
+    }
+
+    public static void toggle() {
+        enabled = !enabled;
     }
 
     public static void put(Identifier id, LightyMode<?, ?> mode) {
+        // the first registered mode becomes the default mode for now,
+        // later maybe remember the last mode the user chose.
+        // Requires: Config stuff
+        if (MODES.isEmpty()) {
+            ModeLoader.mode = mode;
+        }
         MODES.put(id, mode);
     }
 
     @Nullable
     public static LightyMode<?, ?> getCurrentMode() {
+        if (!enabled) return null;
         return mode;
     }
 
