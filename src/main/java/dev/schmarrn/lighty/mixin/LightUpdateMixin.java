6ea38306ffdd85c10032ec0556a38b80e4ef605a
andi-makes <andi@andi-makes.dev>
2023-06-06 17:13:42 +0200

    Change mixin entrypoint to be in ClientChunkCache

--- src/main/java/dev/schmarrn/lighty/mixin/LightUpdateMixin.java
+++ src/main/java/dev/schmarrn/lighty/mixin/LightUpdateMixin.java
@@ -1,20 +1,18 @@
 package dev.schmarrn.lighty.mixin;
 
 import dev.schmarrn.lighty.event.Compute;
-import net.minecraft.client.multiplayer.ClientLevel;
-import net.minecraft.client.multiplayer.ClientPacketListener;
+import net.minecraft.client.multiplayer.ClientChunkCache;
 import net.minecraft.core.SectionPos;
-import net.minecraft.network.protocol.game.ClientboundBlockUpdatePacket;
+import net.minecraft.world.level.LightLayer;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(ClientPacketListener.class)
+@Mixin(ClientChunkCache.class)
 public class LightUpdateMixin {
-    @Inject(method = "handleBlockUpdate", at = @At("TAIL"))
-    private void lighty$onBlockUpdate(ClientboundBlockUpdatePacket clientboundBlockUpdatePacket, CallbackInfo ci) {
-        Compute.updateSubChunkAndSurrounding(SectionPos.of(clientboundBlockUpdatePacket.getPos()));
+    @Inject(method = "onLightUpdate", at = @At("TAIL"))
+    private void lighty$onBlockUpdate(LightLayer lightLayer, SectionPos sectionPos, CallbackInfo ci) {
+        Compute.updateSubChunk(sectionPos);
     }
 }
