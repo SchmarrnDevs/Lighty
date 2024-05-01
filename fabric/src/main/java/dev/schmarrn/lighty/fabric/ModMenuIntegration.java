f70533ee3fd3226f322702f5d7a5eff5c5c10435
poq <120293384+poqli@users.noreply.github.com>
2024-05-01 13:25:01 -0400

    Changed default screen for Lighty settings
    Changed the default settings screen from "ModeSwitcherScreen" to "SettingsScreen" to match the changes made in the latest Lighty version and to allow for easier porting to it

--- fabric/src/main/java/dev/schmarrn/lighty/fabric/ModMenuIntegration.java
+++ fabric/src/main/java/dev/schmarrn/lighty/fabric/ModMenuIntegration.java
@@ -2,11 +2,11 @@ package dev.schmarrn.lighty.fabric;
 
 import com.terraformersmc.modmenu.api.ConfigScreenFactory;
 import com.terraformersmc.modmenu.api.ModMenuApi;
-import dev.schmarrn.lighty.ui.ModeSwitcherScreen;
+import dev.schmarrn.lighty.ui.SettingsScreen;
 
 public class ModMenuIntegration implements ModMenuApi {
     @Override
     public ConfigScreenFactory<?> getModConfigScreenFactory() {
-        return ModeSwitcherScreen::new;
+        return SettingsScreen::new;
     }
 }
