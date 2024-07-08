03cd416295fc954895bc0275574714eb6226db9c
andi-makes <andi@andi-makes.dev>
2024-07-08 13:03:35 +0200

    Better Mixin Location
    Fixes issues with Hammers/Excavators

--- common/src/main/java/dev/schmarrn/lighty/mixin/BlockPlacedMixin.java
+++ common/src/main/java/dev/schmarrn/lighty/mixin/BlockPlacedMixin.java
@@ -1,19 +1,20 @@
 package dev.schmarrn.lighty.mixin;
 
 import dev.schmarrn.lighty.event.Compute;
-import net.minecraft.client.multiplayer.ClientLevel;
+import net.minecraft.client.renderer.LevelRenderer;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.SectionPos;
+import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.block.state.BlockState;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(ClientLevel.class)
+@Mixin(LevelRenderer.class)
 public class BlockPlacedMixin {
-    @Inject(method = "setBlock", at = @At("HEAD"))
-    private void lighty$onPlace(BlockPos blockPos, BlockState blockState, int i, int j, CallbackInfoReturnable<Boolean> cir) {
+    @Inject(method = "blockChanged", at = @At("HEAD"))
+    private void lighty$onPlace(BlockGetter blockGetter, BlockPos blockPos, BlockState blockState, BlockState blockState2, int i, CallbackInfo ci) {
         // TODO Nice to have: Only execute on blocks that don't change the lightmap
         // On the other Hand: We are just inserting into a HashMap, which should be cheap enough to don't care
         Compute.updateSubChunk(SectionPos.of(blockPos));
